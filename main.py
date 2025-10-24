f3558fc0b3fa9846ba5e48e97b036d067780840b
Shivam-1409 <shivampuri1409@gmail.com>
2025-10-24 12:12:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from PIL import Image
 import joblib
 from tensorflow.keras.models import load_model
 
+import os
+os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
+
 model = load_model("model.h5", compile=False)
 CLASS_NAMES = ['Early Blight','Late Blight','Healthy']
 
@@ -33,4 +36,5 @@ if uploaded_file is not None:
             st.success(f"🎯 Model Prediction: {CLASS_NAMES[predicted_index]}")
         except Exception as e:
             st.error(f"❌ Error during prediction: {e}")
+
 
